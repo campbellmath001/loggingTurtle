70c805f6d8bcd4bca21d0d643eb7b8ea342c289b
terrapin <terrapin@solanologgingturtle>
2024-07-10 19:46:51 -0700

    check for necessary directories and create if needed

---
+++ LoggingTurtle.py
@@ -0,0 +1,28 @@
+
+#this will be the folder name where the csv files are stored
+#html files stored at '~/public_html/entityName/
+entityName = "FFPD"
+
+
+from pathlib import Path
+
+# Get the current working directory
+current_dir = Path.cwd()
+home_dir = Path.home()
+
+# Check if the subdirectory exists
+csv_Path = current_dir / entityName
+if not csv_Path.exists():
+    # Create the subdirectory if it doesn't exist
+    csv_Path.mkdir(parents=True)
+    print(f"Subdirectory '{subdirectory_name}' created successfully at {csv_Path.absolute()}")
+else:
+    print(f"Subdirectory '{subdirectory_name}' already exists at {csv_Path.absolute()}")
+
+html_Path = home_dir / public_html / entityName
+
+if not html_Path.exists():
+    html_Path.mkdir(parents=True)
+    print(f"Subdirectory '{subdirectory_name}' created successfully at {html_Path.absolute()}")
+else:
+    print(f"Subdirectory '{subdirectory_name}' already exists at {html_Path.absolute()}")
