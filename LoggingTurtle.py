cd67a7966d0015460c8552e6259e47a969c1b9d2
Leatherback <Leatherback@SolanoLoggingTurtles>
2024-07-11 22:27:22 -0700

    PEP8 style fixes

--- LoggingTurtle.py
+++ LoggingTurtle.py
@@ -1,21 +1,20 @@
-
-#this will be the folder name where the csv files are stored
-#html files stored at '~/public_html/entityName/
-entityName = "FFPD"
-
 from datetime import datetime, timedelta
 import logging
 from pathlib import Path
+
+# this will be the folder name where the csv files are stored
+# html files stored at '~/public_html/entityName/
+entityName = "FFPD"
 
 # Get the current working directory
 current_dir = Path.cwd()
 home_dir = Path.home()
 
-log_Path = current_dir / "TurtleLogs" / entityName 
+log_Path = current_dir / "TurtleLogs" / entityName
 
-#check if log directory exists
+# check if log directory exists
 if not log_Path.exists():
-    #create the directory
+    # create the directory
     log_Path.mkdir(parents=True)
     print(f"Logging directory created at {log_Path.absolute()}")
 
@@ -23,7 +22,7 @@ logfile = log_Path / f"{datetime.today().strftime('%M-%S-%H-%d-%m-%Y')}.log"
 
 logger = logging.getLogger(__name__)
 
-logging.basicConfig(filename = logfile, level = logging.DEBUG)
+logging.basicConfig(filename=logfile, level=logging.DEBUG)
 
 logger.debug(f'Logging initiated in file {logfile}')
 
@@ -43,12 +42,11 @@ if not html_Path.exists():
 else:
     logger.debug(f'html path at {html_Path.absolute()} verified')
 
-#yesterday sorted by day/month/year for sorting most recent
-yesterday = (datetime.today() - timedelta(days = 1)).strftime('%d-%m-%Y')
+# yesterday sorted by day/month/year for sorting most recent
+yesterday = (datetime.today() - timedelta(days=1)).strftime('%d-%m-%Y')
 
-#where to write csv file and format of filename
+# where to write csv file and format of filename
 csv_File = csv_Path / f"{yesterday}_{entityName}.csv"
 
-#where to write html file and format of filename
+# where to write html file and format of filename
 html_File = html_Path / f"{yesterday}_{entityName}.html"
-
