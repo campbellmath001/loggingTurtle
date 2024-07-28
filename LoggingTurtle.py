1a1dd77e4c4d8cbcfddfba6bdd06065e64e83e7c
terrapin <terrapin@solanologgingturtle>
2024-07-27 23:58:27 -0700

    linting / added name for block parameter in regular expressions

--- LoggingTurtle.py
+++ LoggingTurtle.py
@@ -1,10 +1,11 @@
 from datetime import datetime, timedelta
 from pathlib import Path
-import logging
-import adbc_driver_sqlite.dbapi
-import pandas as pd
 import sys
 import argparse
+import logging
+
+import adbc_driver_sqlite.dbapi
+import pandas as pd
 from tomlkit import dump, load
 from tomlkit import datetime as tk_datetime
 
@@ -22,7 +23,7 @@ args = parser.parse_args()
 
 # load turtles.toml using tomlkit
 # tomlkit represents the file as a tomldocument class which acts like a dict
-with open('turtles.toml') as f:
+with open('turtles.toml', encoding="utf-8") as f:
     entityDict = load(f)
 
 # check that name is a valid entity
@@ -48,13 +49,13 @@ def verify_path(test_path):
         try:
             test_path.mkdir(parents=True)
             print(f"Directory created at {test_path.resolve()}")
-        except Exception as e:
-            print(f'an error has occurred when verifying the file structure: {e}')
+        except FileExistsError as _e:
+            print(f'an error has occurred when verifying the file structure: {_e}')
             sys.exit(1)
 
 # Define the directories where the data will be placed
-entityHome = (home_dir / "loggingTurtle" / entityName)
-html_Path = (home_dir / "public_html" / entityName)
+entityHome = home_dir / "loggingTurtle" / entityName
+html_Path = home_dir / "public_html" / entityName
 
 # Check if in debug mode and create a debug environment if needed
 if args.debug:
@@ -81,24 +82,24 @@ logger = logging.getLogger(__name__)
 
 logging.basicConfig(filename=logfile, level=logging.DEBUG)
 
-logger.debug(f'Logging initiated in file {logfile.resolve()}')
+logger.debug('Logging initiated in file %s', logfile.resolve())
 
-logger.debug(f' File System verified.')
+logger.debug('File System verified.')
 
 # pull the website with tables into a dataframe
 try:
-    tables = pd.read_html(url)
-    logger.debug(f'Url {url} read successful')
+    tables = pd.read_html(url, dtype_backend="pyarrow")
+    logger.debug('Url %s read successful', url)
 except Exception as e:
-    logger.error(f'Error when fetching the url: {e}')
+    logger.error('Error when fetching the url: %s', e)
     sys.exit(1)
 
 # select the correct table for archiving
 try:
     raw_data = tables[1]
-    logger.debug(f'desired table # read successfully')
+    logger.debug('desired table # read successfully')
 except Exception as e:
-    logger.error(f'Error when choosing desired table for logging: {e}')
+    logger.error('Error when choosing desired table for logging: %s', e)
     sys.exit(1)
 
 # clean up the data with a custom method chain for your data
@@ -112,8 +113,8 @@ try:
         )
         .assign(
             Incident_Time=lambda x: pd.to_datetime(x['Incident_Time'], format='%m/%d/%Y %I:%M:%S %p'),
-            Block=lambda x: x['Address'].str.extract(r'(\d+) Block of (?P<street>.+)')[0],
-            Street=lambda x: x['Address'].str.extract(r'(\d+) Block of (?P<street>.+)')['street'],
+            Block=lambda x: x['Address'].str.extract(r'(?P<block>\d+) Block of (?P<street>.+)')['block'],
+            Street=lambda x: x['Address'].str.extract(r'(?P<block>\d+) Block of (?P<street>.+)')['street'],
             Full_Address=lambda x: x['Block'].astype(str) + ' ' + x['Street'] + ' Fairfield, Ca'
         )
         .sort_values(
@@ -122,7 +123,7 @@ try:
     )
     logger.debug('Data Cleaned Successfully')
 except Exception as e:
-    logger.error(f'error when cleaning data with method chain: {e}')
+    logger.error('error when cleaning data with method chain: %s', e)
     # if the data cleanse fails we should exit with an error
     sys.exit(1)
 
@@ -133,9 +134,9 @@ db_File = entityHome / "DataBase" / f"{entityName}.db"
 # ----create connection to database
 try:
     db = adbc_driver_sqlite.dbapi.connect(str(db_File.resolve()))
-    logger.debug(f'Database connected at {db_File.resolve()}')
+    logger.debug('Database connected at %s', str(db_File.resolve()))
 except Exception as e:
-    logger.error(f'an error occurred when connecting to database: {e}')
+    logger.error('an error occurred when connecting to database: %s', e)
 
 # --write to database
 try:
@@ -148,14 +149,14 @@ try:
         raw_data.to_sql('Raw_Data_Log', db, if_exists="append", index=False)
         logger.debug('Database commit of raw data successful')
 except Exception as e:
-    logger.error(f'an error occurred when writing to the database: {e}')
+    logger.error('an error occurred when writing to the database: %s', e)
 
 # --close the database
 try:
     db.close()
     logger.debug('Database Closed')
 except Exception as e:
-    logger.error(f'An error occurred when closing the database: {e}')
+    logger.error('An error occurred when closing the database: %s', e)
 
 # Define yesterday string for file naming
 # day,month,year used to place most recent log at top
@@ -169,7 +170,7 @@ try:
     df.to_csv(csv_File, index=False)
     logger.debug('csv file successfully written')
 except Exception as e:
-    logger.error(f'An error occurred when writing csv file: {e}')
+    logger.error('An error occurred when writing csv file: %s', e)
 
 # where to write html file and format of filename
 html_File = html_Path / f"{yesterday}_{entityName}.html"
@@ -179,4 +180,4 @@ try:
     df.to_html(html_File, index=False)
     logger.debug('html file successfully written')
 except Exception as e:
-    logger.error(f'An error occurred when writing the html file:{e}')
+    logger.error('An error occurred when writing the html file: %s', e)
